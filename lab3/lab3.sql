97125c836249d928a002188367bee35fc7c0eae0
AValeriya <79207820+AValeriya@users.noreply.github.com>
2023-04-09 22:20:33 +0300

    Update lab3.sql

--- lab3/lab3.sql
+++ lab3/lab3.sql
@@ -39,3 +39,32 @@ create table rdev.CompanyBank (
     foreign key (bank_id) references rdev.Bank(id),
     unique (company_id, bank_id)
 );
+
+create table rprod.Company (
+    id number constraint CompanyPk primary key,
+    name varchar2(30),
+    email varchar2(30)
+);
+
+create table rprod.Employee (
+    id number constraint EmployeePk primary key,
+    first_name varchar2(30),
+    last_name varchar2(30),
+    is_manager number(1),
+    is_admin number(1),
+    company_id number references rprod.Company(id)
+);
+
+create table rprod.Bank (
+    id number constraint BankPk primary key,
+    name varchar2(30),
+    email varchar2(30)
+);
+
+create table rprod.CompanyBank (
+    company_id number not null,
+    bank_id number not null,
+    foreign key (company_id) references rprod.Company(id),
+    foreign key (bank_id) references rprod.Bank(id),
+    unique (company_id, bank_id)
+);
