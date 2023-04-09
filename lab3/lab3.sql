f14d87aac4464ea87d3a322b8eec802ea778b4b5
AValeriya <79207820+AValeriya@users.noreply.github.com>
2023-04-10 01:04:30 +0300

    Update lab3.sql

--- lab3/lab3.sql
+++ lab3/lab3.sql
@@ -1,70 +1,72 @@
-create user rdev identified by dev_pass;
-create user rprod identified by prod_pass;
+create tablespace lab3_prod 
+DATAFILE 'lab3_prod.dat' SIZE 10M 
+REUSE AUTOEXTEND ON NEXT 10M MAXSIZE 300M;
 
-grant all privileges to rdev;
-grant all privileges to rprod;
+alter session set "_oracle_script"=TRUE;
 
-create table rdev.Company (
-    id number constraint CompanyPk primary key,
-    name varchar2(30),
-    email varchar2(30)
+create user prod 
+IDENTIFIED BY prod
+DEFAULT TABLESPACE lab3_prod
+QUOTA 300M on lab3_prod;
+
+grant create session to prod;
+grant create any table to prod;
+grant create any trigger to prod;
+grant create any procedure to prod;
+grant create any sequence to prod;
+grant create any view to prod;
+
+create tablespace lab3_dev 
+DATAFILE 'lab3_dev.dat' SIZE 10M 
+REUSE AUTOEXTEND ON NEXT 10M MAXSIZE 300M;
+
+create user dev
+IDENTIFIED BY dev
+DEFAULT TABLESPACE lab3_dev
+QUOTA 300M on lab3_dev;
+
+grant create session to dev;
+grant create any table to dev;
+grant create any trigger to dev;
+grant create any procedure to dev;
+grant create any sequence to dev;
+grant create any view to dev;
+
+
+
+
+create table dev.students(
+    id NUMBER,
+    name VARCHAR2(100),
+    group_id NUMBER,
+    CONSTRAINT students_pk PRIMARY KEY(id)
 );
 
-create table rdev.Employee (
-    id number constraint EmployeePk primary key,
-    first_name varchar2(30),
-    last_name varchar2(30),
-    is_manager number(1),
-    company_id number references rdev.Company(id)
+create table dev.groups(
+    id NUMBER,
+    name VARCHAR2(100),
+    CONSTRAINT groups_pk PRIMARY KEY(id)
 );
 
-create table rdev.PersonalData (
-    id number constraint PersonalDataPk primary key,
-    birth_date date,
-    salary number,
-    employee_id number references rdev.Employee(id),
-    constraint EmployeeConstraint unique (employee_id)
+
+create procedure dev.test_proc as 
+begin
+    dbms_output.put_line('Hello world!');
+end;
+
+create function dev.test_func return number as
+begin
+    return 0;
+end;
+
+create table prod.students(
+    id NUMBER,
+    name VARCHAR2(100),
+    CONSTRAINT students_pk PRIMARY KEY(id)
 );
 
-create table rdev.Bank (
-    id number constraint BankPk primary key,
-    name varchar2(30),
-    email varchar2(30)
-);
-
-create table rdev.CompanyBank (
-    company_id number not null,
-    bank_id number not null,
-    foreign key (company_id) references rdev.Company(id),
-    foreign key (bank_id) references rdev.Bank(id),
-    unique (company_id, bank_id)
-);
-
-create table rprod.Company (
-    id number constraint CompanyPk primary key,
-    name varchar2(30),
-    email varchar2(30)
-);
-
-create table rprod.Employee (
-    id number constraint EmployeePk primary key,
-    first_name varchar2(30),
-    last_name varchar2(30),
-    is_manager number(1),
-    is_admin number(1),
-    company_id number references rprod.Company(id)
-);
-
-create table rprod.Bank (
-    id number constraint BankPk primary key,
-    name varchar2(30),
-    email varchar2(30)
-);
-
-create table rprod.CompanyBank (
-    company_id number not null,
-    bank_id number not null,
-    foreign key (company_id) references rprod.Company(id),
-    foreign key (bank_id) references rprod.Bank(id),
-    unique (company_id, bank_id)
+create table prod.teachers(
+    id NUMBER,
+    name VARCHAR2(100),
+    subject VARCHAR2(100)
 );
